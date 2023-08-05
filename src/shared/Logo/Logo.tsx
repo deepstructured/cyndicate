85d33fa2889d16d304b9421b0bda67b5c2a444b3
Ilya Minkin <shadowpounce@icloud.com>
2023-08-05 21:23:39 +0300

    up

--- src/shared/Logo/Logo.tsx
+++ src/shared/Logo/Logo.tsx
@@ -1,10 +1,22 @@
 import clsx from 'clsx'
 import styles from './Logo.module.scss'
+import { FC } from 'react'
 
-export const Logo = () => {
+interface IProps {
+  animatedLogo: boolean
+}
+
+export const Logo: FC<IProps> = ({ animatedLogo }) => {
   return (
     <div data-start="100%" className={clsx(styles.logo, 'reveal')}>
-      <img src="/images/logo.svg" alt="" />
+      {animatedLogo ? (
+        <video playsInline={true} autoPlay={true} loop={true} muted={true}>
+          <source type="video/webm" src="/videos/logo.webm" />
+          <source type="video/mp4" src="/videos/logo.mp4" />
+        </video>
+      ) : (
+        <img src="/images/logo.svg" alt="" />
+      )}
     </div>
   )
 }
--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
   return (
     <header className={styles.header}>
       <nav>
-        <Logo />
+        <Logo animatedLogo={true} />
         <div data-start="100%" className="social-links reveal">
           {socialsData.map((social, idx) => (
             <SocialLink key={idx} icon={social.icon} href={social.href} />
